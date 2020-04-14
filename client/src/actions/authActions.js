211db39f49658ae6082cd80409c7beecd63bd7a0
Samuel Allen <samuelcallen@me.com>
2020-04-14 18:50:30 +1000

    fixed store and rootReducer issue

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -25,14 +25,18 @@ export const loginUser = userData => dispatch => {
     axios
         .post("/api/users/login", userData)
         .then(res => {
+
             // Save to localStorage
             // Set token to localStorage
             const { token } = res.data;
             localStorage.setItem("jwtToken", token);
+
             // Set token to Auth header
             setAuthToken(token);
+
             // Decode token to get user data
             const decoded = jwt_decode(token);
+
             // Set current user
             dispatch(setCurrentUser(decoded));
         })
@@ -61,10 +65,13 @@ export const setUserLoading = () => {
 
 // Log user out
 export const logoutUser = () => dispatch => {
+    
     // Remove token from local storage
     localStorage.removeItem("jwtToken");
+    
     // Remove auth header for future requests
     setAuthToken(false);
+    
     // Set current user to empty object {} which will set isAuthenticated to false
     dispatch(setCurrentUser({}));
 };
