4dead1fffe963eb43d52fa990fcc32a9f794562b
Samuel Allen <samuelcallen@me.com>
2020-04-14 18:16:06 +1000

    update

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -6,3 +6,16 @@ import {
     SET_CURRENT_USER,
     USER_LOADING
 } from "./types";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+    axios
+        .post("/api/users/register", userData)
+        .then(res => history.push("/login")) // re-direct to login on successful register
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
