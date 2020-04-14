e8bb81ae6cbf8bfec4b3c1ea973c9ed505b58b99
Samuel Allen <samuelcallen@me.com>
2020-04-14 18:56:59 +1000

    update

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -36,7 +36,7 @@ class Register extends Component {
                         </Link>
                         <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                             <h4>
-                                <b>Register</b> below
+                                <b>Register</b>
                             </h4>
                             <p className="grey-text text-darken-1">
                                 Already have an account? <Link to="/login">Log in</Link>
