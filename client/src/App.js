27912c19d6cc135bdf8c33d6133b6e5da535bd51
Samuel Allen <samuelcallen@me.com>
2020-04-14 17:52:31 +1000

    basic landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Audioboard temp landing page</h1>
+        <Navbar />
+        <Landing />
       </div>
     );
   }
