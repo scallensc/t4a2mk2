eb73b92dcf99951286df870577667106db0a2164
Samuel Allen <samuelcallen@me.com>
2020-04-14 15:49:29 +1000

    I did bad things with next, moving to more standard react. Spun up basic server with mongoDB connection, added user schema, login and registration form validations

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    providerId: {
+        type: String,
+        required: false
+    },
+    provider: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = User = mongoose.model("users", UserSchema);
