23844521258ce610bc8014e0bc63d417856898e3
Samuel Allen <samuelcallen@me.com>
2020-04-15 13:12:24 +1000

    testing deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require('path');
 const app = express();
 const cors = require('cors')
 
@@ -37,7 +38,15 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Routes
 app.use("/api/users", users);
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server spun up at http://localhost:${port} !`));
